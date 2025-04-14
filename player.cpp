982cb41d25bf345ee0066d740f054e5021a8784e
SahibAlMowt <sahibshukurlu2006@gmail.com>
2025-04-14 21:32:31 +0400

    start_working_with_player_h

--- player.cpp
+++ player.cpp
@@ -1,25 +1,44 @@
 #include "player.h"
 #include "cell.h"
 
-Player::Player(const QString& name)
-    : name(name), money(1500), position(0), bankrupt(false) {}
+Player::Player(const QString &name): name(name), money(1500), position(0), bankrupt(false) {}
 
-void Player::move(int steps, int boardSize) {
+void Player::move(int steps, int boardSize)
+{
     position = (position + steps) % boardSize;
 }
 
-void Player::adjustMoney(int amount) {
+void Player::adjustMoney(int amount)
+{
     money += amount;
-    if (money < 0) {
+
+    if (money < 0)
+    {
         bankrupt = true;
     }
 }
 
-void Player::addProperty(Cell* property) {
+void Player::addProperty(Cell* property)
+{
     properties.push_back(property);
 }
 
-const QString& Player::getName() const { return name; }
-int Player::getMoney() const { return money; }
-int Player::getPosition() const { return position; }
-bool Player::isBankrupt() const { return bankrupt; }
+const QString &Player::getName() const
+{
+    return name;
+}
+
+int Player::getMoney() const
+{
+    return money;
+}
+
+int Player::getPosition() const
+{
+    return position;
+}
+
+bool Player::isBankrupt() const
+{
+    return bankrupt;
+}
--- player.h
+++ player.h
@@ -6,20 +6,24 @@
 
 class Cell;
 
-class Player {
+class Player
+{
+
 public:
-    Player(const QString& name);
+
+    Player(const QString &name);
 
     void move(int steps, int boardSize);
     void adjustMoney(int amount);
     void addProperty(Cell* property);
 
-    const QString& getName() const;
+    const QString &getName() const;
     int getMoney() const;
     int getPosition() const;
     bool isBankrupt() const;
 
 private:
+
     QString name;
     int money;
     int position;
